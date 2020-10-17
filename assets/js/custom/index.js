e64dab619d73663ed9e19fa8045742e22a491b85
riazkhan186 <riazuddinbappy52@gmail.com>
2020-10-17 06:07:54 +0600

    completed

--- assets/js/custom/index.js
+++ assets/js/custom/index.js
@@ -1,5 +1,4 @@
 (function ($) {
-
     $(document).ready(function () {
         // for sticky navbar
         $(window).scroll(function () {
@@ -11,6 +10,27 @@
             }
         });
     });
+
+      // menu bar hide on click li
+      $(document).ready(function () {
+
+        $('.navbar-toggler').click(function () {
+            // $('.navbar-toggler').addClass("collapsed");
+            // $(".navbar-collapse").toggle();
+            $('.collapse').toggle();
+
+        });
+    });
+
+    $('.navbar-collapse a').click(function () {
+        // $('.navbar-toggler').addClass("collapsed");
+        $(".navbar-collapse").toggle();
+        // $('.collapse').toggle();
+        // $(".navbar-collapse").collapse('hide');
+
+    });
+
+
      // barfiller
      $(document).ready(function () {
         $('#bar1').barfiller({
@@ -33,6 +53,26 @@
             barColor: "#DFB871",
             duration: 2000
         });
+        $('#home3-bar1').barfiller({
+            tooltip: false,
+            barColor: "#F06529",
+            duration: 2000
+        });
+        $('#home3-bar2').barfiller({
+            tooltip: false,
+            barColor: "#F06529",
+            duration: 2000
+        });
+        $('#home3-bar3').barfiller({
+            tooltip: false,
+            barColor: "#F06529",
+            duration: 2000
+        });
+        $('#home3-bar4').barfiller({
+            tooltip: false,
+            barColor: "#F06529",
+            duration: 2000
+        });
     });
     $(document).ready(function () {
 
@@ -43,15 +83,39 @@
         });
 
 
-       
+        // home two
+        $('.project-slider').owlCarousel({
+            center:true,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            responsive : {
+                0:{
+                    items: 1,
+                },
+                768:{
+                    items: 2,
+                },
+                992: {
+                    items: 2.8,
+                }
+            }
+        });
+        $('.feedback-slider').owlCarousel({
+            center:true,
+            items: 1,
+            loop: true,
+            // autoplay: true,
+            // autoplayTimeout: 4000,
+            autoplayHoverPause: true,
+            
 
-
-
-
-
-
-
-
+        });
+        //   venobox for video
+        $(document).ready(function(){
+            $('.venobox').venobox(); 
+        });
 
     });
 })(jQuery)
